85f521ef711c7e933d201c64eb5f3a0959835191
cwbcarry <1226207408@qq.com>
2022-01-27 09:32:17 +0800

    48

---
+++ src/array/旋转图像48.java
@@ -0,0 +1,16 @@
+package array;
+
+public class 旋转图像48 {
+    public void rotate(int[][] matrix) {
+        int n = matrix.length;
+        for(int row = 0;row<n/2;row++){
+            for(int col = 0;col<(n+1)/2;col++){
+                int temp = matrix[row][col];
+                matrix[row][col] = matrix[n-col-1][row];
+                matrix[n-col-1][row] = matrix[n-row-1][n-col-1];
+                matrix[n-row-1][n-col-1] = matrix[col][n-row-1];
+                matrix[col][n-row-1] = temp;
+            }
+        }
+    }
+}
