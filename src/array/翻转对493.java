3aaf5f5d7a47a83ae3a19686bb2240735f2d70a3
cwb <1226207408@qq.com>
2022-04-02 10:56:14 +0800

    315，493

---
+++ src/array/翻转对493.java
@@ -0,0 +1,51 @@
+package array;
+
+/**
+ * @author CWB
+ * @date 2022/4/2
+ * @apiNote
+ */
+public class 翻转对493 {
+    int[] temp;
+    int count;
+    public int reversePairs(int[] nums) {
+        int len=nums.length;
+        temp = new int[len];
+        sort(nums,0,len-1);
+        return count;
+    }
+    void sort(int[] nums,int lo,int hi){
+        if(lo==hi){
+            return;
+        }
+        int mid=lo+(hi-lo)/2;
+        sort(nums,lo,mid);
+        sort(nums,mid+1,hi);
+        merge(nums,lo,mid,hi);
+    }
+    void merge(int[] nums,int lo,int mid,int hi){
+        for(int i=lo;i<=hi;i++){
+            temp[i]=nums[i];
+        }
+        int end=mid+1;
+        for(int i=lo;i<=mid;i++){
+            while(end<=hi && (long)nums[i] > (long)nums[end]*2){
+                end++;
+            }
+            count += end - mid -1;
+        }
+        int i=lo;
+        int j=mid+1;
+        for(int p=lo;p<=hi;p++){
+            if(i==mid+1){
+                nums[p]=temp[j++];
+            }else if(j==hi+1){
+                nums[p]=temp[i++];
+            }else if(temp[i] >temp[j]){
+                nums[p]=temp[j++];
+            }else{
+                nums[p]=temp[i++];
+            }
+        }
+    }
+}
---
+++ src/array/计算右侧小于当前元素的个数315.java
@@ -0,0 +1,66 @@
+package array;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * @author CWB
+ * @date 2022/4/2
+ * @apiNote
+ */
+public class 计算右侧小于当前元素的个数315 {
+    private class Pair{
+        int val;
+        int index;
+        Pair(int val,int index){
+            this.val=val;
+            this.index=index;
+        }
+    }
+    int[] count;
+    Pair[] temp;
+    public List<Integer> countSmaller(int[] nums) {
+        int len=nums.length;
+        temp = new Pair[len];
+        count = new int[len];
+        Pair[] arr = new Pair[len];
+        for(int i=0;i<len;i++){
+            arr[i] = new Pair(nums[i],i);
+        }
+        sort(arr,0,len-1);
+        List<Integer> res = new LinkedList<>();
+        for(int num:count){
+            res.add(num);
+        }
+        return res;
+    }
+    void sort(Pair[] nums,int lo,int hi){
+        if(lo==hi){
+            return;
+        }
+        int mid=lo+(hi-lo)/2;
+        sort(nums,lo,mid);
+        sort(nums,mid+1,hi);
+        merge(nums,lo,mid,hi);
+    }
+    void merge(Pair[] nums,int lo,int mid,int hi){
+        for(int i=lo;i<=hi;i++){
+            temp[i]=nums[i];
+        }
+        int i=lo;
+        int j=mid+1;
+        for(int p=lo;p<=hi;p++){
+            if(i==mid+1){
+                nums[p]=temp[j++];
+            }else if(j==hi+1){
+                nums[p]=temp[i++];
+                count[nums[p].index]+=j-mid-1;
+            }else if(temp[i].val>temp[j].val){
+                nums[p]=temp[j++];
+            }else{
+                nums[p]=temp[i++];
+                count[nums[p].index]+=j-mid-1;
+            }
+        }
+    }
+}
