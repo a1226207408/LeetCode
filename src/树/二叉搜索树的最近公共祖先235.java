96f9d8533c3f387ecca3f8c5e9bc377795acdcee
cwb <1226207408@qq.com>
2022-04-10 09:44:38 +0800

    235代码优化、236

--- src/树/二叉搜索树的最近公共祖先235.java
+++ src/树/二叉搜索树的最近公共祖先235.java
@@ -2,14 +2,20 @@ package 树;
 
 public class 二叉搜索树的最近公共祖先235 {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        if ((root.val > p.val && root.val < q.val) || (root.val < p.val && root.val > q.val)) {
-            return root;
-        } else if (root.val == p.val || root.val == q.val) {
-            return root;
-        } else if (root.val > p.val) {
-            return lowestCommonAncestor(root.left, p, q);
-        } else {
-            return lowestCommonAncestor(root.right, p, q);
+        int val1 = Math.min(p.val,q.val);
+        int val2 = Math.max(p.val,q.val);
+        return find(root,val1,val2);
+    }
+    TreeNode find(TreeNode root,int val1,int val2){
+        if(root==null){
+            return null;
         }
+        if(root.val>val2){
+            return find(root.left,val1,val2);
+        }
+        if(root.val<val1){
+            return find(root.right,val1,val2);
+        }
+        return root;
     }
 }
